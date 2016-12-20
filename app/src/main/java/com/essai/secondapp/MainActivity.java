e0ab8bd4ef483dee7a4881afd210ba0f0824c39b
bahriAY <ay@et.esiea.fr>
2016-12-20 01:28:50 +0100

    First

--- app/src/main/java/com/essai/secondapp/MainActivity.java
+++ app/src/main/java/com/essai/secondapp/MainActivity.java
@@ -7,26 +7,18 @@ import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.graphics.Color;
-import android.icu.text.SimpleDateFormat;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.Locale;
-
-import static android.R.attr.button;
 
 public class MainActivity extends AppCompatActivity {
 
